2dca40e27efde5453c4b990defc02d169575170b
diari842 <dragushadiar016@gmail.com>
2025-03-18 19:18:09 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" <?php language_attributes(); ?>>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DS Theme</title>
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>> 
-    <header>
-       <h1><?php bloginfo('name'); ?></h1>
-       <nav>
-        <?php wp_nav_menu(array['theme_locatio' => 'main-menu']) ?>
-       </nav>
-    </header> 
+<?php
+wp_nav_menu(array(
+    'theme_location' => 'main-menu',
+    'menu_class'     => 'nav-menu',
+    'container'      => 'nav',
+    'container_class'=> 'main-navigation'
+));
+?>
