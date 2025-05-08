8c97496ebb940f151bd3ebc70f535c4d797a987d
diari842 <dragushadiar016@gmail.com>
2025-05-08 19:17:13 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,202 +1,141 @@
+<?php get_header();  ?>
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height ?>" 
+width="<?php echo get_custom_header()->width ?>" alt=""> 
+
+<!---->
+
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            
+            <section class="home-blog">
+                <div class="container">
+                    <div class="blog-items">
+                        <?php
+                        if(have_posts()):
+                            while(have_posts()): the_post();
+                                ?>
+                                <article>
+                                     <h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
+                                     <a href="<?php the_permalink();?>"><?php the_post_thumbnail(array(275,275)); ?></a> 
+                                      <div class="meta-info">    
+                                       <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                       <p>Categories: <?php the_category(); ?></p>
+                                       <p>Tags: <?php the_tags(); ?></p>
+                                      </div> 
+                                   <?php the_excerpt(); ?>
+                                </article>
+                                <?php 
+                            endwhile;
+                        else: ?>
+                           <p>Nothing to be displayed</p>
+                        <?php endif; ?>
+                    </div>
+                    <?php get_sidebar(); ?>
+                </div>
+            </section>
+        </main>
+    </div>
+</div>
+<!---->
+<?php get_footer(); ?>
+
+
+
 <?php
 
+require get_template_directory() . '/inc/customizer.php';
 
-add_action('widgets_init', 'dstheme_sidebars');
-function dstheme_sidebars(){
-    register_sidebar(
+function wpdevs_load_scripts(){
+    wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
+    wp_enqueue_style( 'google-fonts', '
+https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap
+', array(), null );
+    wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
+
+function wpdevs_config(){
+    register_nav_menus(
         array(
-            'name' => 'Blog Sidebar',
-            'id' => 'sidebar-blog',
-            'description' => 'This is the Blog Sidebar. You can add your widgets here.',
-            'before_widget' => '<div class = "widget-title">',
-            'after_widget' => '</div>',
-            'before_title' => '<h4 class = "widget-title">',
-            'after_title' => '</h4>'
+            'wp_devs_main_menu' => 'Main Menu',
+            'wp_devs_footer_menu' => 'Footer Menu'
         )
     );
+
+    $args = array(
+        'height'    => 225,
+        'width'     => 1920
+    );
+    add_theme_support( 'custom-header', $args );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'custom-logo', array(
+        'width' => 200,
+        'height'    => 110,
+        'flex-height'   => true,
+        'flex-width'    => true
+    ) );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ));
+    add_theme_support( 'title-tag' );
+    //add_theme_support ('align-wide');
+    add_theme_support( 'responsive-embeds' );
+    add_theme_support( 'editor-styles ' );
+    add_editor_style( 'style-editor.css' );
+    add_theme_support( 'wp-block-styles' );
+}
+add_action( 'after_setup_theme', 'wpdevs_config', 0 );
+
+add_action( 'widgets_init', 'wpdevs_sidebars' );
+function wpdevs_sidebars(){
+    register_sidebar(
+        array(
+            'name'  => 'Blog Sidebar',
+            'id'    => 'sidebar-blog',
+            'description'   => 'This is the Blog Sidebar. You can add your widgets here.',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4 class="widget-title">',
+            'after_title'   => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name'  => 'Service 1',
+            'id'    => 'services-1',
+            'description'   => 'First Service Area',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4 class="widget-title">',
+            'after_title'   => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name'  => 'Service 2',
+            'id'    => 'services-2',
+            'description'   => 'Second Service Area',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4 class="widget-title">',
+            'after_title'   => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name'  => 'Service 3',
+            'id'    => 'services-3',
+            'description'   => 'Third Service Area',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4 class="widget-title">',
+            'after_title'   => '</h4>'
+        )
+    );
+}
+
+if ( ! function_exists( 'wp_body_open' ) ){
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
 } 
-
-register_sidebar(
-    array(
-        'name' => 'Blog Sidebar',
-        'id' => 'sidebar-blog',
-        'description' => 'This is the Blog Sidebar. You can add your widgets here.',
-        'before_widget' => '<div class = "widget-title">',
-        'after_widget' => '</div>',
-        'before_title' => '<h4 class = "widget-title">',
-        'after_title' => '</h4>'
-    )
-); 
-
-
-
-
-
-
-<h2>Services</h2>
-                        <div class="container">
-                            <div class="service-item">
-                                <?php
-                                    if( is_active_sidebar( 'services-1' )){
-                                        dynamic_sidebar( 'services-1' );
-                                    }
-                                ?>
-                            </div>
-                            <div class="service-item">
-                                <?php
-                                    if( is_active_sidebar( 'services-2' )){
-                                        dynamic_sidebar( 'services-2' );
-                                    }
-                                ?>
-                            </div>
-                            <div class="service-item">
-                                <?php
-                                    if( is_active_sidebar( 'services-3' )){
-                                        dynamic_sidebar( 'services-3' );
-                                    }
-                                ?>
-                            </div>
-                        </div> 
-
-
-
-                        <?php get_header(); ?>
-        <div id="content" class="site-content">
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main">
-                    <section class="hero">
-                        Hero
-                    </section>
-                    <section class="services">
-                        <h2>Services</h2>
-                        <div class="container">
-                            <div class="services-item">
-                                <?php
-                                    if( is_active_sidebar( 'sevices-1' )){
-                                        dynamic_sidebar( 'sevices-1' );
-                                    }
-                                ?>
-                            </div>
-                            <div class="services-item">
-                                <?php
-                                    if( is_active_sidebar( 'services-2' )){
-                                        dynamic_sidebar( 'services-2' );
-                                    }
-                                ?>
-                            </div>
-                            <div class="services-item">
-                                <?php
-                                    if( is_active_sidebar( 'services-3' )){
-                                        dynamic_sidebar( 'services-3' );
-                                    }
-                                ?>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="home-blog">
-                        <div class="container">
-                            <div class="blog-items">
-                                <?php
-                                if( have_posts() ):
-                                    while( have_posts() ): the_post();
-                                        ?>
-                                            <article>
-                                                <h2><?php the_title(); ?></h2>
-                                                <div class="meta-info">
-                                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link();?></p>
-                                                    <p>Catgeries: <?php the_category(); ?> </p>
-                                                    <p> <?php the_tags(); ?></p>
-                                                </div>
-                                                <?php the_content(); ?>
-                                            </article>
-                                        <?php
-                                    endwhile;
-                                else: ?>
-                                    <p>Nothing to be displayed!</p>          
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </section>
-                </main>
-            </div>
-        </div>
-<?php get_footer(); ?> 
-
-
-<?php get_header(); ?>
-        <div id="content" class="site-content">
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main">
-                    <section class="hero">
-                        Hero
-                    </section>
-                    <section class="services">
-                        <h2>Services</h2>
-                        <div class="container">
-                            <div class="services-item">
-                                <?php
-                                    if( is_active_sidebar( 'sevices-1' )){
-                                        dynamic_sidebar( 'sevices-1' );
-                                    }
-                                ?>
-                            </div>
-                            <div class="services-item">
-                                <?php
-                                    if( is_active_sidebar( 'services-2' )){
-                                        dynamic_sidebar( 'services-2' );
-                                    }
-                                ?>
-                            </div>
-                            <div class="services-item">
-                                <?php
-                                    if( is_active_sidebar( 'services-3' )){
-                                        dynamic_sidebar( 'services-3' );
-                                    }
-                                ?>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="home-blog">
-                        <h2>Latest News</h2>
-                        <div class="container">
-                            <?php
-
-                            $args = array(
-                                'post_type' => 'post',
-                                'posts_per_page' => 3,
-                                'category_in' => array( 5,19,20 ),
-                                'category_not_in' => array( 1 )
-                            );
-
-                            $postlist = new WP_Query( $args );
-
-
-                            if( $postlist->have_posts() ):
-                                while( $postlist->have_posts() ): $postlist->the_post();
-                                    ?>
-                                        <article class = "latest-news">
-                                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a> 
-                                            <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a> 
-                                            <div class="meta-info">
-                                               <p>
-                                                   by <span><?php the_author_posts_link(); ?></span>
-                                                   Categories: <span><?php the_category( ' ' ) ?></span>
-                                                   Tags: <?php the_tags('', ', '); ?>
-                                               </p>
-                                               <p><span><?php echo get_the_date();?></span></p>
-                                            </div>
-                                            <?php the_excerpt(); ?>
-                                        </article>
-                                    <?php
-                                endwhile;
-                                wp_reset_postdata();
-                            else: ?>
-                                <p>Nothing to be displayed!</p>          
-                            <?php endif; ?>
-                        </div>
-                    </section>
-                </main>
-            </div>
-        </div>
-<?php get_footer(); ?> 
-?>
