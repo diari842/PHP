a8d5a6101a64452c36bad0377517c50194643f7b
diari842 <dragushadiar016@gmail.com>
2025-05-12 16:03:34 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,144 +1,48 @@
-<?php
-require get_template_directory() . '/inc/customizer.php';
-function wpdevs_load_scripts(){
-    wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
-    wp_enqueue_style( 'google-fonts', '
-https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap
-', array(), null );
-    wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true );
+if( function_exists('acf_add_local_field_group') ) {
+
+    acf_add_local_field_group(array(
+        'key' => 'group_homepage_content',
+        'title' => 'Homepage Content',
+        'fields' => array(
+            array(
+                'key' => 'field_hero_image',
+                'label' => 'Hero Image',
+                'name' => 'hero_image',
+                'type' => 'file',
+                'instructions' => 'Upload a hero image (JPG, PNG, WebP).',
+                'required' => 1,
+                'return_format' => 'array',
+                'library' => 'all',
+                'mime_types' => 'jpg,jpeg,png,webp',
+            ),
+            array(
+                'key' => 'field_hero_text',
+                'label' => 'Hero Text',
+                'name' => 'hero_text',
+                'type' => 'wysiwyg',
+                'instructions' => 'Enter the text to display over the hero image.',
+                'required' => 1,
+                'tabs' => 'all',
+                'toolbar' => 'full',
+                'media_upload' => 0,
+                'delay' => 0,
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'page_type',
+                    'operator' => '==',
+                    'value' => 'front_page',
+                ),
+            ),
+        ),
+        'style' => 'default',
+        'position' => 'normal',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'active' => true,
+        'description' => '',
+    ));
+
 }
-add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
-
-function wpdevs_config(){
-    register_nav_menus(
-        array(
-            'wp_devs_main_menu' => 'Main Menu',
-            'wp_devs_footer_menu' => 'Footer Menu'
-        )
-    );
-
-    $args = array(
-        'height'    => 225,
-        'width'     => 1920
-    );
-    add_theme_support( 'custom-header', $args );
-    add_theme_support( 'post-thumbnails' );
-    add_theme_support( 'custom-logo', array(
-        'width' => 200,
-        'height'    => 110,
-        'flex-height'   => true,
-        'flex-width'    => true
-    ) );
-}
-add_action( 'after_setup_theme', 'wpdevs_config', 0 );
-
-add_action( 'widgets_init', 'wpdevs_sidebars' );
-function wpdevs_sidebars(){
-    register_sidebar(
-        array(
-            'name'  => 'Blog Sidebar',
-            'id'    => 'sidebar-blog',
-            'description'   => 'This is the Blog Sidebar. You can add your widgets here.',
-            'before_widget' => '<div class="widget-wrapper">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name'  => 'Service 1',
-            'id'    => 'services-1',
-            'description'   => 'First Service Area',
-            'before_widget' => '<div class="widget-wrapper">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name'  => 'Service 2',
-            'id'    => 'services-2',
-            'description'   => 'Second Service Area',
-            'before_widget' => '<div class="widget-wrapper">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'
-        )
-    );
-    register_sidebar(
-        array(
-            'name'  => 'Service 3',
-            'id'    => 'services-3',
-            'description'   => 'Third Service Area',
-            'before_widget' => '<div class="widget-wrapper">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'
-        )
-    );
-}
-
-if(! function_exists('wp_body_open')){
-    function wp_body_open(){
-        do_action('wp_body_open');
-    }
-} 
-// 3. Blog
-$wp_customize->add_section( 
-    'sec_blog', 
-    array(
-        'title' => 'Blog Section'
-) );
-
-        // Posts per page
-        $wp_customize->add_setting( 
-            'set_per_page', 
-            array(
-                'type' => 'theme_mod',
-                'sanitize_callback' => 'absint'
-        ) );
-
-        $wp_customize->add_control( 
-            'set_per_page', 
-            array(
-                'label' => 'Posts per page',
-                'description' => 'How many items to display in the post list?',			
-                'section' => 'sec_blog',
-                'type' => 'number'
-        ) );
-
-        // Post categories to include
-        $wp_customize->add_setting( 
-            'set_category_include', 
-            array(
-                'type' => 'theme_mod',
-                'sanitize_callback' => 'sanitize_text_field'
-        ) );
-
-        $wp_customize->add_control( 
-            'set_category_include', 
-            array(
-                'label' => 'Post categories to include',
-                'description' => 'Comma separated values or single category ID',
-                'section' => 'sec_blog',
-                'type' => 'text'
-        ) );	
-
-        // Post categories to exclude
-        $wp_customize->add_setting( 
-            'set_category_exclude', 
-            array(
-                'type' => 'theme_mod',
-                'sanitize_callback' => 'sanitize_text_field'
-        ) );
-
-        $wp_customize->add_control( 
-            'set_category_exclude', 
-            array(
-                'label' => 'Post categories to exclude',
-                'description' => 'Comma separated values or single category ID',			
-                'section' => 'sec_blog',
-                'type' => 'text'
-        ) );
